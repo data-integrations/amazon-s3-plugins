61768ef62fba91a14cbeae7189c34ce47cdc30b4
Terence Yim <terencey@google.com>
2019-04-04 15:14:48 -0700

    Rename from co.cask to io.cdap

--- src/main/java/io/cdap/plugin/aws/s3/source/S3BatchSource.java
+++ src/main/java/io/cdap/plugin/aws/s3/source/S3BatchSource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015-2016 Cask Data, Inc.
+ * Copyright © 2015 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -14,20 +14,23 @@
  * the License.
  */
 
-package co.cask.aws.s3.sink;
+package io.cdap.plugin.aws.s3.source;
 
-import co.cask.cdap.api.annotation.Description;
-import co.cask.cdap.api.annotation.Macro;
-import co.cask.cdap.api.annotation.Name;
-import co.cask.cdap.api.annotation.Plugin;
-import co.cask.cdap.etl.api.batch.BatchSink;
-import co.cask.cdap.etl.api.batch.BatchSinkContext;
-import co.cask.hydrator.common.LineageRecorder;
-import co.cask.hydrator.format.plugin.AbstractFileSink;
-import co.cask.hydrator.format.plugin.AbstractFileSinkConfig;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import io.cdap.cdap.api.annotation.Description;
+import io.cdap.cdap.api.annotation.Macro;
+import io.cdap.cdap.api.annotation.Name;
+import io.cdap.cdap.api.annotation.Plugin;
+import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.api.plugin.EndpointPluginContext;
+import io.cdap.cdap.etl.api.batch.BatchSource;
+import io.cdap.cdap.etl.api.batch.BatchSourceContext;
+import io.cdap.plugin.common.LineageRecorder;
+import io.cdap.plugin.format.FileFormat;
+import io.cdap.plugin.format.input.PathTrackingInputFormat;
+import io.cdap.plugin.format.plugin.AbstractFileSource;
+import io.cdap.plugin.format.plugin.AbstractFileSourceConfig;
 
 import java.lang.reflect.Type;
 import java.util.Collections;
@@ -35,36 +38,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nullable;
+import javax.ws.rs.Path;
 
 /**
- * {@link S3BatchSink} that stores the data of the latest run of an adapter in S3.
+ * A {@link BatchSource} that reads from Amazon S3.
  */
-@Plugin(type = BatchSink.PLUGIN_TYPE)
+@Plugin(type = BatchSource.PLUGIN_TYPE)
 @Name("S3")
 @Description("Batch source to use Amazon S3 as a source.")
-public class S3BatchSink extends AbstractFileSink<S3BatchSink.S3BatchSinkConfig> {
-  private static final String ENCRYPTION_VALUE = "AES256";
+public class S3BatchSource extends AbstractFileSource<S3BatchSource.S3BatchConfig> {
   private static final String S3A_ACCESS_KEY = "fs.s3a.access.key";
   private static final String S3A_SECRET_KEY = "fs.s3a.secret.key";
-  private static final String S3A_ENCRYPTION = "fs.s3a.server-side-encryption-algorithm";
-
   private static final String S3N_ACCESS_KEY = "fs.s3n.awsAccessKeyId";
   private static final String S3N_SECRET_KEY = "fs.s3n.awsSecretAccessKey";
-  private static final String S3N_ENCRYPTION = "fs.s3n.server-side-encryption-algorithm";
   private static final String ACCESS_CREDENTIALS = "Access Credentials";
 
-  private final S3BatchSinkConfig config;
+  @SuppressWarnings("unused")
+  private final S3BatchConfig config;
 
-  public S3BatchSink(S3BatchSinkConfig config) {
+  public S3BatchSource(S3BatchConfig config) {
     super(config);
     this.config = config;
   }
 
   @Override
-  protected Map<String, String> getFileSystemProperties(BatchSinkContext context) {
+  protected Map<String, String> getFileSystemProperties(BatchSourceContext context) {
+    String authenticationMethod = config.authenticationMethod;
     Map<String, String> properties = new HashMap<>(config.getFilesystemProperties());
-
-    if (ACCESS_CREDENTIALS.equalsIgnoreCase(config.authenticationMethod)) {
+    if (authenticationMethod != null && authenticationMethod.equalsIgnoreCase(ACCESS_CREDENTIALS)) {
       if (config.path.startsWith("s3a://")) {
         properties.put(S3A_ACCESS_KEY, config.accessID);
         properties.put(S3A_SECRET_KEY, config.accessKey);
@@ -73,38 +74,45 @@ public class S3BatchSink extends AbstractFileSink<S3BatchSink.S3BatchSinkConfig>
         properties.put(S3N_SECRET_KEY, config.accessKey);
       }
     }
-
-    if (config.shouldEnableEncryption()) {
-      if (config.path.startsWith("s3a://")) {
-        properties.put(S3A_ENCRYPTION, ENCRYPTION_VALUE);
-      } else if (config.path.startsWith("s3n://")) {
-        properties.put(S3N_ENCRYPTION, ENCRYPTION_VALUE);
-      }
+    if (config.shouldCopyHeader()) {
+      properties.put(PathTrackingInputFormat.COPY_HEADER, "true");
     }
     return properties;
   }
 
   @Override
   protected void recordLineage(LineageRecorder lineageRecorder, List<String> outputFields) {
-    lineageRecorder.recordWrite("Write", "Wrote to S3.", outputFields);
-  }
-
-  @VisibleForTesting
-  S3BatchSinkConfig getConfig() {
-    return config;
+    lineageRecorder.recordRead("Read", "Read from S3.", outputFields);
   }
 
   /**
-   * S3 Sink configuration.
+   * Endpoint method to get the output schema of a source.
+   *
+   * @param config configuration for the source
+   * @param pluginContext context to create plugins
+   * @return schema of fields
+   */
+  @Path("getSchema")
+  public Schema getSchema(S3BatchConfig config, EndpointPluginContext pluginContext) {
+    FileFormat fileFormat = config.getFormat();
+    if (fileFormat == null) {
+      return config.getSchema();
+    }
+    Schema schema = fileFormat.getSchema(config.getPathField());
+    return schema == null ? config.getSchema() : schema;
+  }
+
+  /**
+   * Config class that contains properties needed for the S3 source.
    */
   @SuppressWarnings("unused")
-  public static class S3BatchSinkConfig extends AbstractFileSinkConfig {
+  public static class S3BatchConfig extends AbstractFileSourceConfig {
     private static final Gson GSON = new Gson();
     private static final Type MAP_STRING_STRING_TYPE = new TypeToken<Map<String, String>>() { }.getType();
 
     @Macro
-    @Description("The S3 path where the data is stored. Example: 's3a://logs' for " +
-      "S3AFileSystem or 's3n://logs' for S3NativeFileSystem.")
+    @Description("Path to file(s) to be read. If a directory is specified, terminate the path name with a '/'. " +
+      "The path must start with s3a:// or s3n://.")
     private String path;
 
     @Macro
@@ -120,30 +128,25 @@ public class S3BatchSink extends AbstractFileSink<S3BatchSink.S3BatchSinkConfig>
     @Macro
     @Nullable
     @Description("Authentication method to access S3. " +
-      "Defaults to Access Credentials. URI scheme should be s3a:// or s3n://.")
+      "Defaults to Access Credentials. URI scheme should be s3a:// for S3AFileSystem or s3n:// for " +
+      "S3NativeFileSystem.")
     private String authenticationMethod;
 
     @Macro
     @Nullable
-    @Description("Server side encryption. Defaults to True. " +
-      "Sole supported algorithm is AES256.")
-    private Boolean enableEncryption;
-
-    @Macro
-    @Nullable
-    @Description("Any additional properties to use when reading from the filesystem. "
-      + "This is an advanced feature that requires knowledge of the properties supported by the underlying filesystem.")
+    @Description("Any additional properties to use when reading from the filesystem. " +
+      "This is an advanced feature that requires knowledge of the properties supported by the underlying filesystem.")
     private String fileSystemProperties;
 
-    S3BatchSinkConfig() {
-      // Set default value for Nullable properties.
-      this.enableEncryption = false;
-      this.authenticationMethod = ACCESS_CREDENTIALS;
-      this.fileSystemProperties = GSON.toJson(Collections.emptyMap());
+    public S3BatchConfig() {
+      authenticationMethod = ACCESS_CREDENTIALS;
+      fileSystemProperties = GSON.toJson(Collections.emptyMap());
     }
 
+    @Override
     public void validate() {
-      if (ACCESS_CREDENTIALS.equalsIgnoreCase(authenticationMethod)) {
+      super.validate();
+      if (ACCESS_CREDENTIALS.equals(authenticationMethod)) {
         if (!containsMacro("accessID") && (accessID == null || accessID.isEmpty())) {
           throw new IllegalArgumentException("The Access ID must be specified if " +
                                                "authentication method is Access Credentials.");
@@ -153,19 +156,15 @@ public class S3BatchSink extends AbstractFileSink<S3BatchSink.S3BatchSinkConfig>
                                                "authentication method is Access Credentials.");
         }
       }
-
-      if (!containsMacro("path") && !path.startsWith("s3a://") && !path.startsWith("s3n://")) {
-        throw new IllegalArgumentException("Path must start with s3a:// or s3n://.");
+      if (!containsMacro("path") && (!path.startsWith("s3a://") && !path.startsWith("s3n://"))) {
+        throw new IllegalArgumentException("Path must start with s3a:// for S3AFileSystem or s3n:// for " +
+                                             "S3NativeFilesystem.");
       }
     }
 
     @Override
     public String getPath() {
       return path;
-    }
-
-    boolean shouldEnableEncryption() {
-      return enableEncryption;
     }
 
     Map<String, String> getFilesystemProperties() {
